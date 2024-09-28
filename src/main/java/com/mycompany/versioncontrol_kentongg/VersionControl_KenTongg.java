919719dafe4566eba6c9511d2f558a0498a51a04
hoang <hoang@Ken>
2024-09-28 05:13:33 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/versioncontrol_kentongg/VersionControl_KenTongg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_kentongg;
+
+/**
+ *
+ * @author hoang
+ */
+public class VersionControl_KenTongg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
